e9461119ec907f4bcde047e31d228ce01560962e
Jithendra2107 <jithendrasai2107@gmail.com>
2026-06-12 06:46:28 +0000

    Initial commit: Terraform S3 project

---
+++ project_s3/main.tf
@@ -0,0 +1,5 @@
+module "aws_s3" {
+  source      = "./modules/aws_s3"
+  bucket_name = "my-unique-bucket-name-669112"
+}
+
---
+++ project_s3/modules/aws_s3/main.tf
@@ -0,0 +1,10 @@
+resource "aws_s3_bucket" "example" {
+   bucket = var.bucket_name
+}
+
+resource "aws_s3_bucket_versioning" "versioning_example" {
+  bucket = aws_s3_bucket.example.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
